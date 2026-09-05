70fa9b927bff2b73f12d89698e7333a633430c31
Steven Miori <smiori@cfbc.org>
2026-09-04 22:41:32 -0500

    Use Klein Cain mark as favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,14 @@ import './globals.css';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://sylvanmiori.github.io/klein-cain-game-day/'),
-  icons: { icon: 'favicon.svg' },
+  icons: {
+    icon: [
+      { url: 'favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: 'favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    shortcut: 'favicon-32x32.png',
+    apple: [{ url: 'apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
+  },
   title: 'Klein Cain vs Oak Ridge | Cain Game Day',
   description: 'The Sept. 4, 2026 game-day briefing for Klein Cain vs Oak Ridge.',
   openGraph: {
